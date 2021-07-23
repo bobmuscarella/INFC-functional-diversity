6dc41968b0d287c9d9787f6252f6d42f7680aec4
roellammerant <roellammerant@users.noreply.github.com>
2021-07-23 21:18:03 +0200

    prelimenary results research question 1

--- CODE/x.CompileData.R
+++ CODE/x.CompileData.R
@@ -126,5 +126,242 @@ VPD
 # Do other climate variables mediate the relationship between FD and *response*?
 
 
+#############################
+#### Research question 1 ####
+#############################
+
+### Option 1: Create one environmental gradient and plot versus CWM/Fdis
+VPD <- plot$vpd
+Soil_Moisture <- plot$soilmoisture
+ord <- prcomp( ~ VPD  +
+                 Soil_Moisture,
+               center = TRUE, scale = TRUE)
+summary(ord)
+
+library("factoextra")
+### visualize the variables used in the PCA
+A1 <- fviz_pca_var(ord, col.var="contrib",gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),repel = TRUE # Avoid text overlapping)
+)
+
+###  visualize results for individuals
+B1 <- fviz_pca_ind(ord,
+                   label = "none", # hide individual labels
+)
+
+### Put the plots together
+PCAplot1 <- ggarrange(A1, B1, labels = c("A", "B"), ncol = 2, nrow = 1)
+PCAplot1
+
+### Coordinates for individuals and plotting it against soil moisture
+res.ind <- get_pca_ind(ord)
+Coord <- res.ind$coord
+
+Dim1 <- Coord[,-c(2)]
+Dim2 <- Coord[,-c(1)]
+
+plot$Dim1 <- Dim1
+plot$Dim2 <- Dim2
+
+## Plotting relationships out
+
+SLA <- lm(plot$cwm_SLA ~ plot$Dim2)
+summary(SLA)
+
+ggplot(data = plot, aes(x = Dim2, y = cwm_SLA)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+Wooddensity <- lm(plot$cwm_StemDensity ~ plot$Dim2)
+summary(Wooddensity)
+
+ggplot(data = plot, aes(x = Dim2, y = cwm_StemDensity)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+Height <- lm(plot$cwm_Height ~ plot$Dim2)
+summary(Height)
+
+ggplot(data = plot, aes(x = Dim2, y = cwm_Height)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+SeedMass <- lm(plot$cwm_SeedMass ~ plot$Dim2)
+summary(SeedMass)
+
+ggplot(data = plot, aes(x = Dim2, y = cwm_SeedMass)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+XylemVulnerability <- lm(plot$cwm_XylemVulnerability ~ plot$Dim2)
+summary(XylemVulnerability)
+
+ggplot(data = plot, aes(x = Dim2, y = cwm_XylemVulnerability)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+FunctionalDispersion <- lm(plot$FDis ~ plot$Dim2)
+summary(FunctionalDispersion)
+
+ggplot(data = plot, aes(x = Dim2, y = FDis)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+# Visualize the the relationship between soil moisture and vdp
+
+ggplot(data = plot, aes(x = vpd, y = soilmoisture)) + geom_point(color='red') 
+
+# Visualize dimension 1, to see where low soil moisture/vpd plots occur
+pacman::p_load(ggplot2, sf, rnaturalearth, rnaturalearthdata, sp, rgeos)
+theme_set(theme_bw()) ## sets background theme
+Italy <- ne_countries(country = 'italy',scale = "medium", returnclass = "sf")
+
+ggplot(data = Italy) + geom_sf() + geom_point(data = plot, aes(LON_ND_W84, LAT_ND_W84, color = Dim1)) + labs(y = "", x = "") + scale_color_gradientn(colours = rainbow(5))
+
+
+
+### Option 2A: PCA species traits + CWM species position PCA 
+
+#TAKE OUT NA VALUES FOR PCA
+Trait_PCA <- na.omit(trait)
+
+StemDensity <- Trait_PCA$StemDensity
+SLA <- Trait_PCA$SLA_log
+Height <- Trait_PCA$Height_log
+SeedMass <- Trait_PCA$SeedMass_log
+XylemVulnerability <- Trait_PCA$XylemVulnerability
+
+ordB <- prcomp( ~ StemDensity  +
+                 SLA +
+                 Height +
+                 SeedMass +
+                 XylemVulnerability,
+               center = TRUE, scale = TRUE)
+summary(ordB)
+
+### visualize the variables used in the PCA
+A2 <- fviz_pca_var(ordB, col.var="contrib",gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),repel = TRUE # Avoid text overlapping)
+)
+
+###  visualize results for plots
+B2 <- fviz_pca_ind(ordB,
+                   label = "none", # hide individual labels
+)
+
+
+### Put the plots together
+PCAplot2 <- ggarrange(A2, B2, labels = c("A", "B"), ncol = 2, nrow = 1)
+PCAplot2
+
+### Contribution of variables
+tres.varB <- get_pca_var(ordB)
+
+contrib2 <- tres.varB$contrib
+contrib2
+
+### Coordinates for individuals and plotting it against soil moisture
+res.indB <- get_pca_ind(ordB)
+CoordB <- res.indB$coord
+
+Dim1 <- CoordB[,-c(2,3,4,5)]
+Dim2 <- CoordB[,-c(1,3,4,5)]
+
+
+Trait_PCA$Dim1 <- Dim1
+Trait_PCA$Dim2 <- Dim2
+
+############# Calculate CWM of PCA position 
+
+cwm_tree_commB <- tree_comm[,colnames(tree_comm) %in% rownames(Trait_PCA)]
+cwm_tree_commB <- cwm_tree_commB[rowSums(cwm_tree_commB) > 0,]
+cwmB <- FD::functcomp(Trait_PCA[,c(11:12)], cwm_tree_commB)
+colnames(cwmB) <- paste0("cwm_", colnames(cwmB))
+PlotB <- cbind(plot, cwmB[match(plot$idpunto, rownames(cwmB)),])
+
+###Take out Na values 
+PlotB <- PlotB[!is.na(PlotB$cwm_Dim1),]
+PlotB <- PlotB[!is.na(PlotB$cwm_Dim2),]
+
+### Plot the CWM versus climate variables
+
+VPD1 <- lm(PlotB$cwm_Dim1 ~ PlotB$vpd)
+summary(VPD1)
+
+ggplot(data = PlotB, aes(x = vpd, y = cwm_Dim1)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+SoilMoisture1 <- lm(PlotB$cwm_Dim1 ~ PlotB$soilmoisture)
+summary(SoilMoisture1)
+
+ggplot(data = PlotB, aes(x = soilmoisture, y = cwm_Dim1)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+VPD2 <- lm(PlotB$cwm_Dim2 ~ PlotB$vpd)
+summary(VPD2)
+
+ggplot(data = PlotB, aes(x = vpd, y = cwm_Dim2)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+SoilMoisture2 <- lm(PlotB$cwm_Dim2 ~ PlotB$soilmoisture)
+summary(SoilMoisture2)
+
+ggplot(data = PlotB, aes(x = soilmoisture, y = cwm_Dim2)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+
+### Option 2B: FDisp + conifer proportion versus climate    #
+
+FDis1 <- lm(plot$FDis ~ plot$vpd)
+summary(FDis1)
+
+ggplot(data = plot, aes(x = vpd, y = FDis)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+Fdis2 <- lm(plot$FDis ~ plot$soilmoisture)
+summary(Fdis2)
+
+ggplot(data = plot, aes(x = soilmoisture, y = FDis)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+### create conifer column
+Conifer <- read_excel("C:/Users/roell/OneDrive/Bureaublad/Conifer.xlsx", 
+                      col_types = c("numeric", "skip", "text"))
+
+tree$conifer <- Conifer$`Conifer (T/F)`[match(tree$SPcod, Conifer$`Species Code`)]
+TRUE. <- subset(tree, conifer == "T")
+FALSE. <- subset(tree, conifer == "F")
+
+TRUE.$number <- 1
+FALSE.$number <- 1
+
+Truetotal <- aggregate(TRUE.$number, by=list(Category=TRUE.$idpunto), FUN=sum)
+Falsetotal <- aggregate(FALSE.$number, by=list(Category=FALSE.$idpunto), FUN=sum)
+
+### add to plot data and create proportion
+plot$NoConiferAmount <- Falsetotal$x[match(plot$idpunto, Falsetotal$Category)]
+plot$ConiferAmount <- Truetotal$x[match(plot$idpunto, Truetotal$Category)]
+
+plot$NoConiferAmount[is.na(plot$NoConiferAmount)] <- 0
+plot$ConiferAmount[is.na(plot$ConiferAmount)] <- 0
+
+plot$ProportionConifer <- plot$ConiferAmount/(plot$ConiferAmount+plot$NoConiferAmount)
+
+###plotting
+
+Conifer1 <- lm(plot$ProportionConifer ~ plot$vpd)
+summary(Conifer1)
+
+ggplot(data = plot, aes(x = vpd, y = ProportionConifer)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+FdisB <- lm(plot$ProportionConifer ~ plot$soilmoisture)
+summary(FdisB)
+
+ggplot(data = plot, aes(x = soilmoisture, y = ProportionConifer)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+
+### Check is species richness is affected by climate like shown by Marco and Angelo
+
+Richness_tree_comm <- tree_comm[,colnames(tree_comm) %in% rownames(trait)]
+Richness_tree_comm <- Richness_tree_comm[rowSums(Richness_tree_comm) > 0,]
+Richness_tree_comm[Richness_tree_comm>0] <-1
+
+Richness <- rowSums(Richness_tree_comm)
+SpeciesRichness <- as.data.frame(Richness)
+setDT(SpeciesRichness, keep.rownames = TRUE)
+names(SpeciesRichness)[1] <- "IDPlot"
+
+plot$SpeciesRichness <- SpeciesRichness$Richness[match(plot$idpunto, SpeciesRichness$IDPlot)]
+
+RichnessA <- lm(plot$SpeciesRichness ~ plot$vpd)
+summary(RichnessA)
+
+ggplot(data = plot, aes(x = vpd, y = SpeciesRichness)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
+RichnessB <- lm(plot$SpeciesRichness ~ plot$soilmoisture)
+summary(RichnessB)
+
+ggplot(data = plot, aes(x = soilmoisture, y = SpeciesRichness)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+
 
 
