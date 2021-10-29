eb5cd7f6c717198bfd506847b7ad6006eb8d9928
angelrita <86369896+angelrita@users.noreply.github.com>
2021-10-29 19:13:09 +0200

    few updates

--- CODE/1.Analysis.R
+++ CODE/1.Analysis.R
@@ -1,6 +1,9 @@
+# load libraries
+library("factoextra")
+library("ggpubr")
+
 # Load data
-data <- read.csv("DATA/Data_for_analysis.csv")
-head(data)
+dataset <- df
 
 ### VARIABLES OF INTEREST
 ### Annual increment
@@ -39,23 +42,27 @@ head(data)
 #############################
 
 ### Option 1: Create one environmental gradient and plot versus CWM/Fdis
-VPD <- data$vpd
-Soil_Moisture <- data$soilmoisture
+VPD <- dataset$vpd
+Soil_Moisture <- dataset$soilmoisture
 ord <- prcomp( ~ VPD  +
                  Soil_Moisture,
                center = TRUE, scale = TRUE)
 summary(ord)
 
-library("factoextra")
-library("ggpubr")
+
 ### visualize the variables used in the PCA
-A1 <- fviz_pca_var(ord, col.var="contrib",gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),repel = TRUE # Avoid text overlapping)
-)
+A1 <-
+  fviz_pca_var(
+    ord,
+    col.var = "contrib",
+    gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+    repel = TRUE # Avoid text overlapping)
+  )
 
 ###  visualize results for individuals
 B1 <- fviz_pca_ind(ord,
-                   label = "none", # hide individual labels
-)
+                   label = "none" # hide individual labels
+                   )
 
 ### Put the plots together
 PCAplot1 <- ggarrange(A1, B1, labels = c("A", "B"), ncol = 2, nrow = 1)
@@ -64,57 +71,55 @@ PCAplot1
 ### Coordinates for individuals and plotting it against soil moisture
 res.ind <- get_pca_ind(ord)
 Coord <- res.ind$coord
+head(Coord)
+# get dimensions
+Dim1 <- Coord[,1]
+Dim2 <- Coord[,2]
 
-Dim1 <- Coord[,-c(2)]
-Dim2 <- Coord[,-c(1)]
-
-data$Dim1 <- Dim1
-data$Dim2 <- Dim2
+dataset$Dim1 <- Dim1
+dataset$Dim2 <- Dim2
 
 ## Plotting relationships out
+SLA.mod <- lm(cwm_SLA_log ~ Dim2, data=dataset)
+summary(SLA.mod)
+plot(cwm_SLA_log~Dim2, data=dataset, col='red', pch='.')
+abline(SLA.mod)
 
-SLA <- lm(data$cwm_SLA_log ~ data$Dim2)
-summary(SLA)
+Wooddensity.mod <- lm(cwm_StemDensity ~ Dim2, data = dataset)
+summary(Wooddensity.mod)
+plot(cwm_StemDensity~Dim2, data=dataset, col='red', pch='.')
+abline(Wooddensity.mod)
 
-ggplot(data = data, aes(x = Dim2, y = cwm_SLA_log)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+Height.mod <- lm(cwm_Height_log ~ Dim2, data=dataset)
+summary(Height.mod)
+plot(cwm_Height_log~Dim2, data=dataset, col='red', pch='.')
+abline(Height.mod)
 
-Wooddensity <- lm(data$cwm_StemDensity ~ plot$Dim2)
-summary(Wooddensity)
+SeedMass.mod <- lm(cwm_SeedMass_log ~ Dim2, data=dataset)
+summary(SeedMass.mod)
+plot(cwm_SeedMass_log~Dim2, data=dataset, col='red', pch='.')
+abline(SeedMass.mod)
 
-ggplot(data = data, aes(x = Dim2, y = cwm_StemDensity)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
+XylemVulnerability.mod <- lm(cwm_XylemVulnerability ~ Dim2, data=dataset)
+summary(XylemVulnerability.mod)
+plot(cwm_XylemVulnerability~Dim2, data=dataset, col='red', pch='.')
+abline(XylemVulnerability.mod)
 
-Height <- lm(data$cwm_Height_log ~ data$Dim2)
-summary(Height)
+FunctionalDispersion.mod <- lm(FDis ~ Dim2, data=dataset) # Warning !! FDis in not in the dataframe; do you mean FDis_All ??
+summary(FunctionalDispersion.mod)
 
-ggplot(data = data, aes(x = Dim2, y = cwm_Height_log)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
-
-SeedMass <- lm(data$cwm_SeedMass_log ~ data$Dim2)
-summary(SeedMass)
-
-ggplot(data = data, aes(x = Dim2, y = cwm_SeedMass_log)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
-
-XylemVulnerability <- lm(data$cwm_XylemVulnerability ~ data$Dim2)
-summary(XylemVulnerability)
-
-ggplot(data = data, aes(x = Dim2, y = cwm_XylemVulnerability)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
-
-FunctionalDispersion <- lm(data$FDis ~ data$Dim2)
-summary(FunctionalDispersion)
-
-ggplot(data = data, aes(x = Dim2, y = FDis)) + geom_point(color='red') + geom_smooth(method = "lm", se = TRUE)
 
 # Visualize the the relationship between soil moisture and vdp
-
-ggplot(data = data, aes(x = vpd, y = soilmoisture)) + geom_point(color='red') 
+ggplot(data = dataset, aes(x = vpd, y = soilmoisture)) + geom_point(color='red') 
 
 # Visualize dimension 1, to see where low soil moisture/vpd plots occur
 pacman::p_load(ggplot2, sf, rnaturalearth, rnaturalearthdata, sp, rgeos)
 theme_set(theme_bw()) ## sets background theme
 Italy <- ne_countries(country = 'italy',scale = "medium", returnclass = "sf")
+ggplot(data = Italy) + geom_sf() + geom_point(data = dataset, aes(LON_ND_W84, LAT_ND_W84, color = Dim1)) + labs(y = "", x = "") + scale_color_gradientn(colours = rainbow(5))
 
-ggplot(data = Italy) + geom_sf() + geom_point(data = data, aes(LON_ND_W84, LAT_ND_W84, color = Dim1)) + labs(y = "", x = "") + scale_color_gradientn(colours = rainbow(5))
 
-
+# AR: I stopped here with the coding..
 
 ### Option 2A: PCA species traits + CWM species position PCA 
 
