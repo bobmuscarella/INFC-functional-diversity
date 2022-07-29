9c442b749f7ed2f0563a263f65650aa7745f4e81
angelrita <86369896+angelrita@users.noreply.github.com>
2022-07-29 11:37:58 +0200

    Amended Fig 5

--- CODE/Fig_5.R
+++ CODE/Fig_5.R
@@ -5,37 +5,37 @@
 ##%######################################################%##
 pdf(
   "output_plot/Fig_5.pdf",
-  width = 6,
+  width = 5,
   height = 3.5
 )
 
 layout(matrix(c(1,2,3,4,5,6,7,8,9,10,11,12), 2,6, byrow = T))
 
-par(mar = c(0,0,0,0), oma = c(0.5,4,0.5,1), las =1)
+par(mar = c(0,0,0,0), oma = c(0.5,5,0.5,3), las =1)
 
-par(mar = c(2.5,0,0,0)) #Add a space in the bottom axis
+par(mar = c(4.5,0,0,0)) #Add a space in the bottom axis
 
 
 mypch<-21
-mycols<-c('black','dodgerblue', 'indianred')
+mycols<-c('dodgerblue', 'indianred')
 myylim<-c(-0.1,0.3)
-mylabs<-c("P", "T", "M")
+mylabs<-c( "T", "M")
 
 value<-c(
-  parameterEstimates(fit1.a,standardized = TRUE)[3,12],
+  #parameterEstimates(fit1.a,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.a,standardized = TRUE)[3,13],
   parameterEstimates(fit1mg.a,standardized = TRUE)[16,13])
 low<-c(
-  parameterEstimates(fit1.a,standardized = TRUE)[3,9],
+  #parameterEstimates(fit1.a,standardized = TRUE)[3,9],
   parameterEstimates(fit1mg.a,standardized = TRUE)[3,11],
   parameterEstimates(fit1mg.a,standardized = TRUE)[16,11])
 upp<-c(
-  parameterEstimates(fit1.a,standardized = TRUE)[3,10],
+  #parameterEstimates(fit1.a,standardized = TRUE)[3,10],
   parameterEstimates(fit1mg.a,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.a,standardized = TRUE)[16,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -47,34 +47,34 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(FDis[SeedMass]), # x axis label
   ylim=myylim,
-  ylab=expression(F[Dis])
+  ylab=expression(beta)
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
+# axis(side=1,at=1:2,  ## add custom x-axis
+#      label=mylabs)
 abline(h=0, lty=2, col='grey')
-# legend('topleft', legend=c("Pooled", "Temperate", "Mediterranean"),
-#        col=mycols, pch = mypch, box.lty=0, inset=0.02)
+legend('topleft', legend=c("Temperate", "Mediterranean"),
+        col=mycols, pch = 16, box.lty=0, inset=0.02, cex = .5)
 
 #######################################################################
 value<-c(
-  parameterEstimates(fit1.b,standardized = TRUE)[3,12],
+  #parameterEstimates(fit1.b,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.b,standardized = TRUE)[3,13],
   parameterEstimates(fit1mg.b,standardized = TRUE)[16,13])
 low<-c(
-  parameterEstimates(fit1.b,standardized = TRUE)[3,9],
+  #parameterEstimates(fit1.b,standardized = TRUE)[3,9],
   parameterEstimates(fit1mg.b,standardized = TRUE)[3,11],
   parameterEstimates(fit1mg.b,standardized = TRUE)[16,11])
 upp<-c(
-  parameterEstimates(fit1.b,standardized = TRUE)[3,10],
+  #parameterEstimates(fit1.b,standardized = TRUE)[3,10],
   parameterEstimates(fit1mg.b,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.b,standardized = TRUE)[16,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -85,32 +85,30 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(FDis[Height]),
   ylim=myylim,
   ylab='',yaxt='n'
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 
 #######################################################################
 value<-c(
-  parameterEstimates(fit1.c,standardized = TRUE)[3,12],
+  #parameterEstimates(fit1.c,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.c,standardized = TRUE)[3,13],
   parameterEstimates(fit1mg.c,standardized = TRUE)[16,13])
 low<-c(
-  parameterEstimates(fit1.c,standardized = TRUE)[3,9],
+  #parameterEstimates(fit1.c,standardized = TRUE)[3,9],
   parameterEstimates(fit1mg.c,standardized = TRUE)[3,11],
   parameterEstimates(fit1mg.c,standardized = TRUE)[16,11])
 upp<-c(
-  parameterEstimates(fit1.c,standardized = TRUE)[3,10],
+  #parameterEstimates(fit1.c,standardized = TRUE)[3,10],
   parameterEstimates(fit1mg.c,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.c,standardized = TRUE)[16,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -121,31 +119,29 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(FDis[SLA]),
   ylim=myylim,
   ylab='',yaxt='n'
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 #######################################################################
 value<-c(
-  parameterEstimates(fit1.d,standardized = TRUE)[3,12],
+  #parameterEstimates(fit1.d,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.d,standardized = TRUE)[3,13],
   parameterEstimates(fit1mg.d,standardized = TRUE)[16,13])
 low<-c(
-  parameterEstimates(fit1.d,standardized = TRUE)[3,9],
+  #parameterEstimates(fit1.d,standardized = TRUE)[3,9],
   parameterEstimates(fit1mg.d,standardized = TRUE)[3,11],
   parameterEstimates(fit1mg.d,standardized = TRUE)[16,11])
 upp<-c(
-  parameterEstimates(fit1.d,standardized = TRUE)[3,10],
+  #parameterEstimates(fit1.d,standardized = TRUE)[3,10],
   parameterEstimates(fit1mg.d,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.d,standardized = TRUE)[16,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -156,31 +152,29 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(FDis[WD]),
   ylim=myylim,
   ylab='',yaxt='n'
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 #######################################################################
 value<-c(
-  parameterEstimates(fit1.e,standardized = TRUE)[3,12],
+  #parameterEstimates(fit1.e,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.e,standardized = TRUE)[3,13],
   parameterEstimates(fit1mg.e,standardized = TRUE)[16,13])
 low<-c(
-  parameterEstimates(fit1.e,standardized = TRUE)[3,9],
+  #parameterEstimates(fit1.e,standardized = TRUE)[3,9],
   parameterEstimates(fit1mg.e,standardized = TRUE)[3,11],
   parameterEstimates(fit1mg.e,standardized = TRUE)[16,11])
 upp<-c(
-  parameterEstimates(fit1.e,standardized = TRUE)[3,10],
+  #parameterEstimates(fit1.e,standardized = TRUE)[3,10],
   parameterEstimates(fit1mg.e,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.e,standardized = TRUE)[16,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -191,31 +185,29 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(FDis[Xylem]),
   ylim=myylim,
   ylab='',yaxt='n'
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 #######################################################################
 value<-c(
-  parameterEstimates(fit1.f,standardized = TRUE)[3,12],
+  #parameterEstimates(fit1.f,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.f,standardized = TRUE)[3,13],
   parameterEstimates(fit1mg.f,standardized = TRUE)[16,13])
 low<-c(
-  parameterEstimates(fit1.f,standardized = TRUE)[3,9],
+  #parameterEstimates(fit1.f,standardized = TRUE)[3,9],
   parameterEstimates(fit1mg.f,standardized = TRUE)[3,11],
   parameterEstimates(fit1mg.f,standardized = TRUE)[16,11])
 upp<-c(
-  parameterEstimates(fit1.f,standardized = TRUE)[3,10],
+  #parameterEstimates(fit1.f,standardized = TRUE)[3,10],
   parameterEstimates(fit1mg.f,standardized = TRUE)[3,12],
   parameterEstimates(fit1mg.f,standardized = TRUE)[16,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -226,14 +218,12 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(FDis[all]),
   ylim=myylim,
   ylab='',yaxt='n'
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 
 ############################################################
@@ -242,22 +232,22 @@ myylim<-c(-0.15,0.3)
 
 
 value<-c(
-  parameterEstimates(fit1.a,standardized = TRUE)[2,12],
+  #parameterEstimates(fit1.a,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.a,standardized = TRUE)[2,13],
   parameterEstimates(fit1mg.a,standardized = TRUE)[15,13])
 
 low<-c(
-  parameterEstimates(fit1.a,standardized = TRUE)[2,9],
+  #parameterEstimates(fit1.a,standardized = TRUE)[2,9],
   parameterEstimates(fit1mg.a,standardized = TRUE)[2,11],
   parameterEstimates(fit1mg.a,standardized = TRUE)[15,11])
 
 upp<-c(
-  parameterEstimates(fit1.a,standardized = TRUE)[2,10],
+  #parameterEstimates(fit1.a,standardized = TRUE)[2,10],
   parameterEstimates(fit1mg.a,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.a,standardized = TRUE)[15,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -269,35 +259,33 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(CWM[SeedMass]),
   ylim=myylim,
-  ylab=expression(CWM)
+  ylab=expression(beta)
   
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 
 
 
 ############################################################
 value<-c(
-  parameterEstimates(fit1.b,standardized = TRUE)[2,12],
+  #parameterEstimates(fit1.b,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.b,standardized = TRUE)[2,13],
   parameterEstimates(fit1mg.b,standardized = TRUE)[15,13])
 low<-c(
-  parameterEstimates(fit1.b,standardized = TRUE)[2,9],
+  #parameterEstimates(fit1.b,standardized = TRUE)[2,9],
   parameterEstimates(fit1mg.b,standardized = TRUE)[2,11],
   parameterEstimates(fit1mg.b,standardized = TRUE)[15,11])
 upp<-c(
-  parameterEstimates(fit1.b,standardized = TRUE)[2,10],
+  #parameterEstimates(fit1.b,standardized = TRUE)[2,10],
   parameterEstimates(fit1mg.b,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.b,standardized = TRUE)[15,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -309,32 +297,30 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(CWM[Height]),
   ylim=myylim,
   ylab='', yaxt = 'n'
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 
 ############################################################
 value<-c(
-  parameterEstimates(fit1.c,standardized = TRUE)[2,12],
+  #parameterEstimates(fit1.c,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.c,standardized = TRUE)[2,13],
   parameterEstimates(fit1mg.c,standardized = TRUE)[15,13])
 low<-c(
-  parameterEstimates(fit1.c,standardized = TRUE)[2,9],
+  #parameterEstimates(fit1.c,standardized = TRUE)[2,9],
   parameterEstimates(fit1mg.c,standardized = TRUE)[2,11],
   parameterEstimates(fit1mg.c,standardized = TRUE)[15,11])
 upp<-c(
-  parameterEstimates(fit1.c,standardized = TRUE)[2,10],
+  #parameterEstimates(fit1.c,standardized = TRUE)[2,10],
   parameterEstimates(fit1mg.c,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.c,standardized = TRUE)[15,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -346,33 +332,31 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(CWM[SLA]),
   ylim=myylim,
   ylab='', yaxt = 'n',
   
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 
 ############################################################
 value<-c(
-  parameterEstimates(fit1.d,standardized = TRUE)[2,12],
+  #parameterEstimates(fit1.d,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.d,standardized = TRUE)[2,13],
   parameterEstimates(fit1mg.d,standardized = TRUE)[15,13])
 low<-c(
-  parameterEstimates(fit1.d,standardized = TRUE)[2,9],
+  #parameterEstimates(fit1.d,standardized = TRUE)[2,9],
   parameterEstimates(fit1mg.d,standardized = TRUE)[2,11],
   parameterEstimates(fit1mg.d,standardized = TRUE)[15,11])
 upp<-c(
-  parameterEstimates(fit1.d,standardized = TRUE)[2,10],
+  #parameterEstimates(fit1.d,standardized = TRUE)[2,10],
   parameterEstimates(fit1mg.d,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.d,standardized = TRUE)[15,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -384,33 +368,31 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(CWM[WD]),
   ylim=myylim,
   ylab='', yaxt = 'n',
   
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 
 ############################################################
 value<-c(
-  parameterEstimates(fit1.e,standardized = TRUE)[2,12],
+  #parameterEstimates(fit1.e,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.e,standardized = TRUE)[2,13],
   parameterEstimates(fit1mg.e,standardized = TRUE)[15,13])
 low<-c(
-  parameterEstimates(fit1.e,standardized = TRUE)[2,9],
+  #parameterEstimates(fit1.e,standardized = TRUE)[2,9],
   parameterEstimates(fit1mg.e,standardized = TRUE)[2,11],
   parameterEstimates(fit1mg.e,standardized = TRUE)[15,11])
 upp<-c(
-  parameterEstimates(fit1.e,standardized = TRUE)[2,10],
+  #parameterEstimates(fit1.e,standardized = TRUE)[2,10],
   parameterEstimates(fit1mg.e,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.e,standardized = TRUE)[15,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -422,33 +404,31 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(CWM[Xylem]),
   ylim=myylim,
   ylab='', yaxt = 'n',
   
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
 abline(h=0, lty=2, col='grey')
 
 ############################################################
 value<-c(
-  parameterEstimates(fit1.f,standardized = TRUE)[2,12],
+  #parameterEstimates(fit1.f,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.f,standardized = TRUE)[2,13],
   parameterEstimates(fit1mg.f,standardized = TRUE)[15,13])
 low<-c(
-  parameterEstimates(fit1.f,standardized = TRUE)[2,9],
+  #parameterEstimates(fit1.f,standardized = TRUE)[2,9],
   parameterEstimates(fit1mg.f,standardized = TRUE)[2,11],
   parameterEstimates(fit1mg.f,standardized = TRUE)[15,11])
 upp<-c(
-  parameterEstimates(fit1.f,standardized = TRUE)[2,10],
+  #parameterEstimates(fit1.f,standardized = TRUE)[2,10],
   parameterEstimates(fit1mg.f,standardized = TRUE)[2,12],
   parameterEstimates(fit1mg.f,standardized = TRUE)[15,12])
 
 gplots::plotCI(
-  1:3,
+  1:2,
   value,
   li	= low,
   ui=upp,
@@ -460,15 +440,13 @@ gplots::plotCI(
   gap=0, # distance from symbol to error bar
   sfrac=0.005, # width of error bar as proportion of x plotting region (default 0.01)
   xaxt = 'n',
-  xlim=c(0.5,3.5),
-  xaxp=c(1,3,2), # x-min tick mark, x-max tick mark, number of intervals between tick marks
-  xlab="", # x axis label
+  xlim=c(0.5,2.5),
+  xaxp=c(1,2,1), # x-min tick mark, x-max tick mark, number of intervals between tick marks
+  xlab=expression(CWM[Dim1]),
   ylim=myylim,
-  ylab='', yaxt = 'n',
-  
+  ylab='', yaxt = 'n'
 )
-axis(side=1,at=1:3,  ## add custom x-axis
-     label=mylabs)
+
 abline(h=0, lty=2, col='grey')
 
 
